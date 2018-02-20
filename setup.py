f2ee49344cb2da04929f8e9244c426bd4f332fc1
Luke Frisken <luke.frisken@anca.com>
2018-02-20 11:48:19 +1100

    bump version number to 0.7.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class Tox(TestCommand):
 
 setup(
     name='sphinxcontrib-images',
-    version='0.7.1',
+    version='0.7.2',
     url='https://github.com/spinus/sphinxcontrib-images',
     download_url='https://pypi.python.org/pypi/sphinxcontrib-images',
     license='Apache 2',
